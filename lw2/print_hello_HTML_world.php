4efa00e7bb521b70f28257137cd217144eae8b90
romanvmakarov <romanatwork@gmail.com>
2016-06-09 11:22:52 +0300

    добавлено задание 3 (сравнение заголовков)

--- lw2/print_hello_HTML_world.php
+++ lw2/print_hello_HTML_world.php
@@ -1,12 +1,20 @@
 <?php
-  /* 
+/* 
   3. Разработайте PHP приложение print_hello_html.php. Сделайте, чтобы сообщение 
   #5 "Hello HTML World!" выводилось в формате text/html. 
   Сообщение должно располагаться внутри HTML тэга H1.
-  */
+*/
 
   header("Content-type: text/html");
   $headerText = "Hello HTML World!";
   echo "<H1>$headerText</H1>";
   
-  
+/*
+  Определите отличие заголовков ответа сервера для программы PrintHelloHTML от PrintHello.
+  print_hello.php Response Header:
+    Content-Type:text/plain;charset=UTF-8
+  print_hello_HTML_world Response Header:
+    Content-Type:text/html;charset=UTF-8
+  difference is:
+    text/plain vs text/html
+*/
