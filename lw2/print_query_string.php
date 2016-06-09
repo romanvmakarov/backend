3e3187d4e5c0f02166cdecedbbb3bb74a838d5dc
Roman <romanatwork@gmail.com>
2016-06-09 15:11:40 +0300

    добавлено задание 4 (вывод query string)

---
+++ lw2/print_query_string.php
@@ -0,0 +1,18 @@
+<?php
+    header('Content-Type: text/plain');
+    $queryString = $_SERVER['QUERY_STRING'];
+    echo "Query string =\n'$queryString'";
+    
+/*
+  Напишите PHP приложение print_query_string.php, которое выводит данные переданные 
+  #5 в запросе. 
+
+  Пример:
+  Запрос
+  HTTP 1.1 GET /print_query_string.php?my_name=Sarah&my_surname=Rever&message=British+are+coming+by+sea.
+
+  Ответ
+  Content-Type: text/plain
+  Query string = 'my_name=Sarah&my_surname=Rever&message=British+are+coming+by+sea.'
+  
+  */
