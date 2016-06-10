72760f9c491ebe6e8244d4d3cc3ee768ceb84437
Roman <romanatwork@gmail.com>
2016-06-10 10:26:37 +0300

    Добавлены задания 5,6

---
+++ lw2/calc_sum.php
@@ -0,0 +1,38 @@
+<?php
+
+  $arg1 = "";
+  $arg2 = "";
+
+  if (isset($_GET['arg1']))
+  {
+    $arg1=$_GET['arg1'];
+  }
+  else
+  {
+    echo "arg1 is not set<br />";
+  }
+
+  if (isset($_GET['arg2']))
+  {
+    $arg2=$_GET['arg2'];
+  }
+  else
+  {
+    echo "arg2 is not set<br />";
+  }
+
+  $result = $arg1 + $arg2;
+
+  echo $arg1;
+  echo " plus ";
+  echo $arg2;
+  echo " is ";
+  echo $result;
+  
+/*
+  6.  Напишите PHP приложение calc_sum.php, которое вычисляет сумму чисел, переданных в запросе GET.
+  #10 Параметры запроса arg1 и arg2.
+  Например, 
+  /calc_sum.php?arg1=10&arg2=15
+  Выведет на экран число 25.
+*/
