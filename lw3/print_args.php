4de08dadfbd4d032932a908afbd7591bdd7bcd46
Roman <romanatwork@gmail.com>
2016-06-10 15:11:56 +0300

    лабораторная 3, задание 1

---
+++ lw3/print_args.php
@@ -0,0 +1,28 @@
+<?php
+  $quantity = $argc - 1;
+  if ($quantity != 0)
+  {
+    echo "Number of arguments: $quantity".PHP_EOL;
+    echo "Arguments: ";
+    for ($i = 1; $i < $argc; $i++)
+    {
+      echo "$argv[$i] ";
+    }
+  }
+  else
+  {
+    echo "No parameters were specified!";
+  }
+  
+/*
+  1. (#5) Напишите PHP-скрипт print_args.php, выполняющий вывод параметров, переданных в качестве аргументов командной строки, а также их количество. 
+  Программа должна корректно обрабатывать ситуацию, когда параметры не были указаны. В этом случае выводится информационное сообщение “No parameters were specified!”.
+  Внимание! Данная программа запускается из командной строки, а не из браузера.
+
+  Пример:
+  php print_args.php Hello World!
+
+  Вывод:
+  Number of arguments: 2
+  Arguments: Hello World!
+*/
