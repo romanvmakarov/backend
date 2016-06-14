e56e3bc65377b136ad7b8a85e3893e0c410b5b19
Roman <romanatwork@gmail.com>
2016-06-14 12:32:11 +0300

    Лабораторная 4 задание 2

--- lw4/inc/string.inc.php
+++ lw4/inc/string.inc.php
@@ -1,1 +1,14 @@
 <?php
+  function last($str) 
+  {
+    $array = str_split($str);
+    $lastSymbol = array_pop($array);
+    return $lastSymbol;   	
+  }
+
+/*
+  2.  Разработайте функцию last($str). 
+  #10 Функция last принимает строку символов и возвращает лишь последний символ 
+  из этой строки.
+  Функция должна размещаться в файле string.inc.php
+*/
