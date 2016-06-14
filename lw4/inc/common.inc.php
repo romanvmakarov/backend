580da8223b6f05fee87f0874913257837085d127
Roman <romanatwork@gmail.com>
2016-06-14 12:22:53 +0300

    Лабораторная 4 задание 1

---
+++ lw4/inc/common.inc.php
@@ -0,0 +1,9 @@
+<?php
+    require_once ('request.inc.php');
+    require_once ('string.inc.php');
+    
+/*
+  1. Создайте в проекте директорию include. В этой директории создайте
+  #5 файлы common.inc.php, string.inc.php, request.inc.php
+  Подключите файлы string.inc.php и request.inc.php в файле common.inc.php
+*/
